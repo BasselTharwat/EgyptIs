5c3d3580b51a122f94a45ecbc95810ed8ac0e0e3
BasselTharwat <bassel.tharwat01@gmail.com>
2024-12-03 19:46:44 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("lifecycle_version", "2.7.0")
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
